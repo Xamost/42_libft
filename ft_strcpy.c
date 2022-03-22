225420804725989fdf1dbb819ab8f8c282da2c2f
BuildTools <unconfigured@null.spigotmc.org>
2022-03-22 12:21:04 +0100

    suppression de la fonction inutile et ajout de deux

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -2,15 +2,15 @@ char *ft_strcpy(char *dest, const char *src)
 {
     int i;
     i = 0;
-    while(source[i] != '\0')
+    while(src[i] != '\0')
     {
         i++;
     }
     dest = (char *) malloc(i * sizeof(char));
     i = 0;
-    while(source[i] != '\0')
+    while(src[i] != '\0')
     {
-        dest[i] = source[i];
+        dest[i] = src[i];
         i++;
     }
     dest[i] = '\0';
